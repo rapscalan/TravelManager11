6ac6534f442259f06ab7b1b92a3068cc37822dfa
Zak Hyslin <rapscalan@gmail.com>
2019-12-16 16:28:33 -0800

    routes, app test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,4 +17,23 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('creates a trip', () => {
+    return request(app)
+      .post('/api/v1/trips')
+      .send({
+        name: 'Paris',
+        beginDate: new Date('2019-12-20T00:00:00'),
+        endDate: new Date('2019-12-27T00:00:00')
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Paris',
+          beginDate: expect.toEqual('2019-12-20T08:00:00.000Z'),
+          endDate: expect.toEqual('2019-12-27T08:00:00.000Z'),
+          __v: 0
+        });
+      });
+  });
 });
